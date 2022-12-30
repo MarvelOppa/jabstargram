f2855ce4d76619a1b6767c6ad1714d432e776d04
mrbong <sm841004@gmail.com>
2022-12-30 21:50:54 +0900

    로그인 유지

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,13 +1,63 @@
+import { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { useUserState } from '../contexts/UserContext';
 import AuthStack from './AuthStack';
 import MainStack from './MainStack';
+import { onAuthStateChanged } from '../api/auth';
+import { Asset } from 'expo-asset';
+import { initFirebase } from '../api/firebase';
+import * as SplashScreen from 'expo-splash-screen';
 
 const Navigation = () => {
-  const [user] = useUserState();
+  const [user, setUser] = useUserState();
+  const [isReady, setIsReady] = useState(false);
+
+  // useEffect(() => {
+  //   const unsubscribe = onAuthStateChanged((user) => {
+  //     if (user) {
+  //       setUser(user);
+  //     }
+  //     setIsReady(true);
+  //     unsubscribe();
+  //   });
+  // }, [setUser]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Asset.fromModule(
+          require('../../assets/cover.png')
+        ).downloadAsync();
+
+        initFirebase();
+
+        const unsubscribe = onAuthStateChanged((user) => {
+          if (user) {
+            setUser(user);
+          }
+          setIsReady(true);
+          unsubscribe();
+        });
+      } catch (e) {
+        console.log(e);
+        setIsReady(true);
+      }
+    })();
+  }, [setUser]);
+
+  const onReady = async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  };
+
+  if (!isReady) {
+    return null;
+  }
 
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={onReady}>
       {user.uid ? <MainStack /> : <AuthStack />}
     </NavigationContainer>
   );
