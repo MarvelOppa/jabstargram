a8492c5df6c56e21d3809604e1381be992fa0596
mrbong <sm841004@gmail.com>
2023-01-15 17:47:56 +0900

    프로필 페이지, 로그아웃, 프로필 이미지, 유저닉네임

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,20 +1,48 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { Text, StyleSheet, Image, View, Pressable } from 'react-native';
 import { useUserState } from '../contexts/UserContext';
 import { signOut } from '../api/auth';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { GRAY, WHITE } from '../colors';
 
 const ProfileScreen = () => {
   const [user, setUser] = useUserState();
-  console.log(user.uid, user.email, user.displayName, user.photoURL);
+  const { top } = useSafeAreaInsets();
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>ProfileScreen</Text>
-      <Button
-        title={'signout'}
-        onPress={async () => {
-          await signOut();
-          setUser({});
-        }}
-      />
+    <View style={[styles.container, { paddingTop: top }]}>
+      <View style={styles.settingButton}>
+        <Pressable
+          onPress={async () => {
+            signOut();
+            setUser({});
+          }}
+          hitSlop={10}
+        >
+          <MaterialCommunityIcons
+            name="logout-variant"
+            size={24}
+            color={GRAY.DEFAULT}
+          />
+        </Pressable>
+      </View>
+
+      <View style={styles.profile}>
+        <View
+          style={[
+            styles.photo,
+            user.photoURL || { backgroundColor: GRAY.DEFAULT },
+          ]}
+        >
+          <Image source={{ uri: user.photoURL }} style={styles.photo} />
+          <Pressable style={styles.editButton} onPress={() => {}}>
+            <MaterialCommunityIcons name="pencil" size={20} color={WHITE} />
+          </Pressable>
+        </View>
+
+        <Text style={styles.nickname}>{user.displayName || 'nickname'}</Text>
+      </View>
+
+      <View style={styles.listContainer}>{/* 내가 쓴 글 목록 */}</View>
     </View>
   );
 };
@@ -22,12 +50,30 @@ const ProfileScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: WHITE,
+  },
+  settingButton: { paddingHorizontal: 20, alignItems: 'flex-end' },
+  profile: {
+    paddingVertical: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 0.5,
+    borderBottomColor: GRAY.DEFAULT,
+  },
+  photo: { width: 100, height: 100, borderRadius: 50 },
+  editButton: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: GRAY.DARK,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  title: {
-    fontSize: 20,
-  },
+  nickname: { marginTop: 10, fontSize: 24, fontWeight: '600' },
+  listContainer: { flex: 1 },
 });
 
 export default ProfileScreen;
